f91163de176f34e4452e6d15bbed45e868ae97c0
Ed Mackey <elm19087@gmail.com>
2017-03-15 14:28:27 -0400

    Update settings, add stub for importSchema.

---
+++ util/importSchema.js
@@ -0,0 +1,6 @@
+// This NodeJS script imports the official glTF schema files from a
+// folder, and transforms them to make them more functional with vscode's
+// own JsonValidation.  For example, glTF enums are converted to "oneOf" arrays,
+// such that tooltips and auto-complete suggestions are shown for each enum value.
+
+// TODO: Write it
