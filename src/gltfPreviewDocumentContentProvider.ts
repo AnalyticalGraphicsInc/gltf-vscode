a4914179f5315cd9c13f96db9f7e0396fd1ba4ea
Howard Wolosky <HowardWolosky@users.noreply.github.com>
2017-05-12 08:43:05 -0700

    Add preview support for non-embedded glTF
    This will dynamically alter the glTF file content prior to
    loading it up in Cesium so that the uri paths to filenames
    are absolute paths.  That is sufficient to enable Cesium
    to render both glTF and glTF embedded file content.

--- src/gltfPreviewDocumentContentProvider.ts
+++ src/gltfPreviewDocumentContentProvider.ts
@@ -17,6 +17,29 @@ export class GltfPreviewDocumentContentProvider implements TextDocumentContentPr
 
     public provideTextDocumentContent(uri: Uri): string {
         const glTF = vscode.window.activeTextEditor.document.getText();
+
+        // The "uri" property within a glTF file provides the reference to the binary content needed
+        // for the glTF to be processed.  The content of that property is likely to fall into one of
+        // four categories:
+        // 1. Just the binary filename
+        // 2. A partial path to the file.
+        // 3. A full path (or full Uri reference)
+        // 4. The full binary content base-64 encoded with a content-type header.
+        //    a full path specified for their uri.  If they don't have a full path specified,
+        // If we don't make any changes to the glTF content, #3 and #4 work just fine with 3D renderers.
+        // Unfortunately, #1 and #2 are considered to be paths relative to the "pages" subfolder of this
+        // extension, and so those references fail to load.  So, we'll look for any "uri" property that
+        // doesn't have a forward or backward slash in it (even #4 has at least one of those due to the
+        // content-type header), and we'll prepend the root folder that the glTF file content lives in.
+        // This solves for #1, but unfortunately it doesn't yet solve for #2.
+        const glTFRootPath = path.dirname(vscode.window.activeTextEditor.document.fileName);
+        const glTFWithUpdatedPaths = glTF.replace(
+            // Find all uri property entries that don't have a forward or backward slash in them.
+            /^(\s*"uri"\s*:\s*")([^/\\\s]*)(".*)$/igm,
+            // And replace the property content with the root path of where the glTF file currently sits
+            // (making sure that all backslashes are escaped)
+            "$1" + glTFRootPath.replace(/\\/g,"\\\\") + "\\\\$2$3");
+
         const content = `<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -26,7 +49,7 @@ export class GltfPreviewDocumentContentProvider implements TextDocumentContentPr
     <title>glTF Preview</title>
     <link rel="stylesheet" href="${this.getFilePath('pages/previewModel.css')}">
     <script src="${this.getFilePath('Cesium/Cesium.js')}"></script>
-    <script id="glTF" type="text/plain">${glTF}</script>
+    <script id="glTF" type="text/plain">${glTFWithUpdatedPaths}</script>
 </head>
 <body>
     <div id="cesiumContainer">
