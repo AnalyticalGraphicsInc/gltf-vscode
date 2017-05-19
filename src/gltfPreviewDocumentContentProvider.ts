d994ea0d03cce03314987d3c9a0ef03840f60e99
Ed Mackey <elm19087@gmail.com>
2017-05-19 16:33:32 -0400

    Add debug instructions.

--- src/gltfPreviewDocumentContentProvider.ts
+++ src/gltfPreviewDocumentContentProvider.ts
@@ -17,6 +17,21 @@ export class GltfPreviewDocumentContentProvider implements TextDocumentContentPr
         return 'file:///' + this._context.asAbsolutePath(file);
     }
 
+    // Instructions to open Chrome DevTools on the HTML preview window:
+    //
+    // 1. With the HTML preview window open, click Help->Toggle Developer Tools.
+    //    Note that this DevTools is docked and is only for VSCode itself.
+    //
+    // 2. In the Console tab, paste this line:
+    //    document.body.querySelector('webview').getWebContents().openDevTools();
+    //
+    // 3. You now have a second DevTools, the new one is un-docked.  Close the
+    //    old docked one.
+    //
+    // 4. In the top of the Console tab of the remaining un-docked DevTools,
+    //    click the pull-down and change "top" to "_target (webview.html)".
+    //    Now you can debug the HTML preview in the sandboxed iframe.
+
     public provideTextDocumentContent(uri: Uri): string {
         const fileName = decodeURIComponent(uri.authority);
         const document = vscode.workspace.textDocuments.find(e => e.fileName.toLowerCase() === fileName.toLowerCase());
