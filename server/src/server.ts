cf700c4875fe5b4972b4e0af8587b2566611b3d8
Ed Mackey <elm19087@gmail.com>
2017-11-09 16:06:52 -0500

    Code cleanup.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,8 @@
-/* --------------------------------------------------------------------------------------------
- * Some portions copied from lsp-sample:
- * https://github.com/Microsoft/vscode-extension-samples/tree/master/lsp-sample
- * ------------------------------------------------------------------------------------------ */
 'use strict';
 
 import {
     IPCMessageReader, IPCMessageWriter, createConnection, IConnection, TextDocuments, TextDocument,
-    Diagnostic, DiagnosticSeverity, InitializeResult, CompletionItem
-    //TextDocumentPositionParams, CompletionItemKind
+    Diagnostic, DiagnosticSeverity, InitializeResult
 } from 'vscode-languageserver';
 import * as path from 'path';
 import * as Url from 'url';
@@ -21,6 +16,7 @@ let connection: IConnection = createConnection(new IPCMessageReader(process), ne
 // Create a simple text document manager. The text document manager
 // supports full document sync only
 let documents: TextDocuments = new TextDocuments();
+
 // Make the text document manager listen on the connection
 // for open, change and close text document events
 documents.listen(connection);
@@ -37,6 +33,11 @@ function tryGetJsonMap(textDocument: TextDocument) {
     return undefined;
 }
 
+function isLocalGltf(textDocument: TextDocument) {
+    const lowerUri = textDocument.uri.toLowerCase();
+    return (lowerUri.startsWith('file:///') && lowerUri.endsWith('.gltf'));
+}
+
 // After the server has started the client sends an initilize request. The server receives
 // in the passed params the rootPath of the workspace plus the client capabilites.
 connection.onInitialize((): InitializeResult => {
@@ -44,18 +45,8 @@ connection.onInitialize((): InitializeResult => {
         capabilities: {
             // Tell the client that the server works in FULL text document sync mode
             textDocumentSync: documents.syncKind
-            // Tell the client that the server support code complete
-            //completionProvider: {
-            //    resolveProvider: true
-            //}
         }
     }
-});
-
-// The content of a text document has changed. This event is emitted
-// when the text document first opened or when its content has changed.
-documents.onDidChangeContent((change) => {
-    scheduleValidation(change.document);
 });
 
 // The settings interface describe the server relevant settings part
@@ -78,8 +69,31 @@ let currentSettings: GltfSettings;
 //
 connection.onDidChangeConfiguration((change) => {
     currentSettings = <GltfSettings>change.settings.glTF;
-    // Schedule revalidation any open text documents
-    documents.all().forEach(scheduleValidation);
+    if (currentSettings.Validation.enable) {
+        // Schedule revalidation any open text documents
+        documents.all().forEach(scheduleValidation);
+    } else {
+        if (debounceValidateTimer) {
+            clearTimeout(debounceValidateTimer);
+            debounceValidateTimer = undefined;
+        }
+        documents.all().forEach(clearValidationTextDocument);
+    }
+});
+
+// The content of a text document has changed. This event is emitted
+// when the text document first opened or when its content has changed.
+documents.onDidChangeContent(change => {
+    if (currentSettings.Validation.enable) {
+        scheduleValidation(change.document);
+    }
+});
+
+// Turn off validation of closed documents.
+documents.onDidClose(change => {
+    if (currentSettings.Validation.enable) {
+        unscheduleValidation(change.document);
+    }
 });
 
 /**
@@ -87,8 +101,7 @@ connection.onDidChangeConfiguration((change) => {
  * @param textDocument The document to schedule validator for.
  */
 function scheduleValidation(textDocument: TextDocument): void {
-    const lowerUri = textDocument.uri.toLowerCase();
-    if (lowerUri.startsWith('file:///') && lowerUri.endsWith('.gltf')) {
+    if (isLocalGltf(textDocument)) {
         console.log('schedule ' + textDocument.uri);
         if (documentsToValidate.indexOf(textDocument) < 0) {
             documentsToValidate.push(textDocument);
@@ -101,6 +114,22 @@ function scheduleValidation(textDocument: TextDocument): void {
             documentsToValidate.forEach(validateTextDocument);
             documentsToValidate = [];
         }, currentSettings.Validation.debounce);
+    }
+}
+
+function unscheduleValidation(textDocument: TextDocument): void {
+    var index = documentsToValidate.indexOf(textDocument);
+    if (index >= 0) {
+        console.log('un-schedule ' + textDocument.uri);
+        documentsToValidate.splice(index, 1);
+    }
+}
+
+function clearValidationTextDocument(textDocument: TextDocument): void {
+    if (isLocalGltf(textDocument)) {
+        console.log('disable validation ' + textDocument.uri);
+        const diagnostics: Diagnostic[] = [];
+        connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
     }
 }
 
@@ -133,6 +162,13 @@ function validateTextDocument(textDocument: TextDocument): void {
     const gltfData = Buffer.from(textDocument.getText());
     const folderName = path.resolve(fileName, '..');
 
+    const map = tryGetJsonMap(textDocument);
+    if (!map) {
+        let diagnostics: Diagnostic[] = [getDiagnostic({ message: 'Error parsing JSON document.' }, {})];
+        connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
+        return;
+    }
+
     gltfValidator.validateBytes(baseName, new Uint8Array(gltfData), (uri) =>
         new Promise((resolve, reject) => {
             uri = path.resolve(folderName, uri);
@@ -153,13 +189,15 @@ function validateTextDocument(textDocument: TextDocument): void {
         }
     ).then((result) => {
         let diagnostics: Diagnostic[] = [];
-        // TODO: Parse this before calling validator
-        const map = tryGetJsonMap(textDocument);
-        if (!map) {
-            diagnostics.push(getDiagnostic({ message: 'Error parsing JSON document.' }, {}));
-        } else if (result.issues && result.issues.messages) {
+        if (result.issues && result.issues.messages) {
             const messages = result.issues.messages;
-            convertErrorsToDiagnostics(diagnostics, map, messages);
+            const numMessages = messages.length;
+            for (let i = 0; i < numMessages; ++i) {
+                let info = messages[i];
+                if (info.message) {
+                    diagnostics.push(getDiagnostic(info, map));
+                }
+            }
 
             if (result.issues.truncated) {
                 diagnostics.push(getDiagnostic({
@@ -173,23 +211,9 @@ function validateTextDocument(textDocument: TextDocument): void {
         // Validator's error
         console.warn('glTF Validator had problems.');
         console.warn(result);
+        let diagnostics: Diagnostic[] = [getDiagnostic({ message: 'glTF Validator error: ' + result }, {})];
+        connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
     });
-}
-
-connection.onDidChangeWatchedFiles((_change) => {
-    // Monitored files have change in VSCode
-    connection.console.log('We recevied an file change event');
-});
-
-
-function convertErrorsToDiagnostics(diagnostics: Diagnostic[], map: any, messages: any) {
-    let len = messages.length;
-    for (let i = 0; i < len; ++i) {
-        let info = messages[i];
-        if (info.message) {
-            diagnostics.push(getDiagnostic(info, map));
-        }
-    }
 }
 
 function getDiagnostic(info: any, map: any) : Diagnostic {
@@ -235,60 +259,6 @@ function getDiagnostic(info: any, map: any) : Diagnostic {
         source: 'glTF Validator'
     };
 }
-
-/*
-// This handler provides the initial list of the completion items.
-connection.onCompletion((_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
-    // The pass parameter contains the position of the text document in
-    // which code complete got requested. For the example we ignore this
-    // info and always provide the same completion items.
-    return [
-        {
-            label: 'TypeScript',
-            kind: CompletionItemKind.Text,
-            data: 1
-        },
-        {
-            label: 'JavaScript',
-            kind: CompletionItemKind.Text,
-            data: 2
-        }
-    ]
-});
-*/
-
-// This handler resolve additional information for the item selected in
-// the completion list.
-connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
-    if (item.data === 1) {
-        item.detail = 'TypeScript details',
-            item.documentation = 'TypeScript documentation'
-    } else if (item.data === 2) {
-        item.detail = 'JavaScript details',
-            item.documentation = 'JavaScript documentation'
-    }
-    return item;
-});
-
-/*
-connection.onDidOpenTextDocument((params) => {
-    // A text document got opened in VSCode.
-    // params.uri uniquely identifies the document. For documents store on disk this is a file URI.
-    // params.text the initial full content of the document.
-    connection.console.log(`${params.textDocument.uri} opened.`);
-});
-connection.onDidChangeTextDocument((params) => {
-    // The content of a text document did change in VSCode.
-    // params.uri uniquely identifies the document.
-    // params.contentChanges describe the content changes to the document.
-    connection.console.log(`${params.textDocument.uri} changed: ${JSON.stringify(params.contentChanges)}`);
-});
-connection.onDidCloseTextDocument((params) => {
-    // A text document got closed in VSCode.
-    // params.uri uniquely identifies the document.
-    connection.console.log(`${params.textDocument.uri} closed.`);
-});
-*/
 
 // Listen on the connection
 connection.listen();
